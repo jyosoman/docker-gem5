e2a0f25a144413dd5def9dd8674909e3456aa2f2
Jyothish Soman <jyothish@grakn.ai>
2018-09-18 22:36:40 +0100

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Build and Clean in one step to keep image manageable
 
-scons -j$(nproc) --ignore-style build/ARM/gem5.opt
-rm -f /usr/local/bin/gem5.opt
-mv build/ARM/gem5.opt /usr/local/bin
+scons -j4 build/ARM/gem5.fast
+rm -f /usr/local/bin/gem5.fast
+mv build/ARM/gem5.fast /usr/local/bin
 rm -rf build
 mkdir -p build/ARM
-ln -s /usr/local/bin/gem5.opt build/ARM/gem5.opt
+ln -s /usr/local/bin/gem5.fast build/ARM/gem5.fast
